e79875449dc23da4899bd92aa78dfc6fe6742994
Alex Melfi <alex.d.melfi@gmail.com>
2024-04-27 12:10:13 -0400

    Added Bulbapedia querying for large images

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,25 +5,26 @@ const downloadButton = document.getElementById('download-button')
 const pokemonInput = document.getElementById('pokemon-input')
 const display = document.getElementById('display')
 const pokemonImage = document.getElementById('pokemon-image')
+const imageTypeToggle = document.getElementById('image-type-toggle')
 
 // clicking the download button
 downloadButton.addEventListener('click', () => {
-  // image downloader calls the API and downloads an image to the machine.
-  imageDownloader(pokemonInput.value)
-    .then(fileName => {
-      display.textContent = `
+    // image downloader calls the API and downloads an image to the machine.
+    imageDownloader(pokemonInput.value, imageTypeToggle.checked)
+      .then(fileName => {
+        display.textContent = `
       File downloaded as ${fileName}.\n
       Saved to the project /images directory.
       `
 
-      // set pokemon image display
-      pokemonImage.src = `./images/${fileName}`
-    })
-    // error if the image cannot be downloaded or displayed.
-    // error is general in the user view, but the specific error can be found in the developer console.
-    .catch(e => {
-      display.textContent = 'Failed to locate requested Pokémon'
-      pokemonImage.src = ''
-      console.error(e)
-    })
+        // set pokemon image display
+        pokemonImage.src = `./images/${fileName}`
+      })
+      // error if the image cannot be downloaded or displayed.
+      // error is general in the user view, but the specific error can be found in the developer console.
+      .catch(e => {
+        display.textContent = 'Failed to locate requested Pokémon'
+        pokemonImage.src = ''
+        console.error(e)
+      })
 })
