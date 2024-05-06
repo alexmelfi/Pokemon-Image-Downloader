14190de036ede9d920193a1016e5343d334226de
nmelfi235 <nmelfi235@gmail.com>
2024-05-06 19:01:16 -0400

    Started integrating react with typescript to start designing the ui

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,30 +1,34 @@
 const imageDownloader = require(__dirname + '/util/imageDownloader')
 
 // DOM element selectors
-const downloadButton = document.getElementById('download-button')
-const pokemonInput = document.getElementById('pokemon-input')
-const display = document.getElementById('display')
-const pokemonImage = document.getElementById('pokemon-image')
-const imageTypeToggle = document.getElementById('image-type-toggle')
+const downloadButton = document.querySelector('#download-button')
+const pokemonInput = document.querySelector('#pokemon-input')
+const display = document.querySelector('#display')
+const pokemonImage = document.querySelector('#pokemon-image')
+const imageTypeToggle = document.querySelector('#image-type-toggle')
 
 // clicking the download button
 downloadButton.addEventListener('click', () => {
-    // image downloader calls the API and downloads an image to the machine.
-    imageDownloader(pokemonInput.value, imageTypeToggle.checked)
-      .then(fileName => {
-        display.textContent = `
-      File downloaded as ${fileName}.\n
-      Saved to the project /images directory.
-      `
-
-        // set pokemon image display
-        pokemonImage.src = `./images/${fileName}`
-      })
-      // error if the image cannot be downloaded or displayed.
-      // error is general in the user view, but the specific error can be found in the developer console.
-      .catch(e => {
-        display.textContent = 'Failed to locate requested Pokémon'
-        pokemonImage.src = ''
-        console.error(e)
-      })
+    
 })
+
+const getImage = () => {
+  // image downloader calls the API and downloads an image to the machine.
+  imageDownloader(pokemonInput.value, imageTypeToggle.checked)
+  .then(fileName => {
+    display.textContent = `
+  File downloaded as ${fileName}.\n
+  Saved to the project /images directory.
+  `
+
+    // set pokemon image display
+    pokemonImage.src = `./images/${fileName}`
+  })
+  // error if the image cannot be downloaded or displayed.
+  // error is general in the user view, but the specific error can be found in the developer console.
+  .catch(e => {
+    display.textContent = 'Failed to locate requested Pokémon'
+    pokemonImage.src = ''
+    console.error(e)
+  })
+}
